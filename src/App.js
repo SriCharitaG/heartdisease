31cb01f7e4e03c6e6cf11e11cf16c81e5019df57
charitasri <charitasrig@gmail.com>
2024-01-04 14:39:20 +0530

    added navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from 'react-router-dom';
+
+
+const App=() =>(
+  <>
+  <Navbar />
+  </>
+)
+  
+export default App;
