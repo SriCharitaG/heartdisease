bfe5e801b0e7521b2836e72233a7eaf009147afa
charitasri <charitasrig@gmail.com>
2024-01-08 12:57:11 +0530

    changed in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -12,10 +12,10 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span className="sr-only"></span></a>
+            <a className="nav-link" href="/">Analysis <span className="sr-only"></span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/Predict">Predict</a>
+            <a className="nav-link" href="/Predict">FAQ</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/Contact">Contact</a>
