81a7ada5a85f8c8a955404b63428aab636ed2c6d
charitasri <charitasrig@gmail.com>
2024-01-10 22:45:34 +0530

    changed components

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="/Home">Hearthealth</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -12,10 +12,10 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/">Analysis <span className="sr-only"></span></a>
+            <a className="nav-link" href="/Analysis">Analysis <span className="sr-only"></span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/Predict">FAQ</a>
+            <a className="nav-link" href="/FAQ">FAQ</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/Contact">Contact</a>
